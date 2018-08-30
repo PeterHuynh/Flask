89eaa0c010b0795b5d369c62a0ae9d8654158585
Peter Huynh <peter.huynh.99@gmail.com>
2018-08-30 08:20:05 -0600

    flask-hello-world

---
+++ app.py
@@ -0,0 +1,17 @@
+# import the Flask class from the flash package
+from flask import Flask
+
+# create the application object
+app = Flask(__name__)
+
+# user the decorator pattern to link the view function to a url
+@app.route("/")
+@app.route("/hello")
+
+ # define the view using a function, which return a string
+def hello_world():
+    return "Hello, World!"
+
+# start the development server using the run() method
+if __name__ == "__main__":
+    app.run()
